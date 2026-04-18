f450023d8370b86fd77e652b9e1a9dd10fb24318
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-04-18 22:51:14 +0530

    add: added the services code

---
+++ internal/service/token_service.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"rate-limiter-service/internal/model"
+	"rate-limiter-service/internal/pkg/utils"
+	"rate-limiter-service/store"
+	"sync"
+	"time"
+)
+
+type RateLimiterService struct {
+	store store.store
+}
+
+func NewRateLimiterService(s store.store) *RateLimiterService {
+	return &RateLimiterService{store: s}
+}
+
+func (rl *RateLimiterService) Allow(userID string) bool {
+	bucket := rl.store.Get(userID)
+	bucket.mu.Lock()
+	defer bucket.mu.Unlock()
+	now := time.Now()
+	elapsed := now.sub(bucket.lastRefillTime).secods()
+	newTokens := int(elapsed * float64(bucket.refillRate))
+	if newTokens > 0 {
+		bucket.Tokens = utils.Min(bucket.maxTokens, bucket.tokens+newTokens)
+		bucket.lastRefill = now
+	}
+	if bucket.tokens > 0 {
+		bucket.tokens--
+
+	}
+
+}
