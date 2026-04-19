ed5d95d2fb7ce181137a2a483b3daed9c0333994
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-04-19 14:36:23 +0530

    add: added the full code in the redis-store

--- internal/store/redis_store.go
+++ internal/store/redis_store.go
@@ -10,6 +10,44 @@ func NewRedisStore()  *RedisStore{
 	}
 	return &RedisStore{
 		client: rdb,
-		ctx: context.Background()
+		ctx: context.Background(),
 	}
+}
+func (r *RedisStore) Get(userID string) *model.TokenBucket{
+	key := "rate_limit:" + userID
+	data, err := r.client.Get(r.ctx, key) Result()
+    //if not found create new bucket
+	if err == redis.Nil{
+		bucket := &model.TokenBucket{
+			Tokens: 10,
+			maxTokens: 10,
+			RefillRate: 5,
+			LastRefill: time.Now(),
+
+		}
+		r.save(key, bucket)
+		return bucket
+	}
+	if err != nil {
+		return &model.TokenBucket{
+			Tokens:     10,
+			MaxTokens:  10,
+			RefillRate: 5,
+			LastRefill: time.Now(),
+		}
+	}
+	var bucket model.TokenBucket
+	json.Unmarshal([]byte(data), &bucket)
+	return &bucket
+}
+
+func (r *RedisStore) save(userID string, bucket *model.TokenBucket){
+	key := "rate_limit:" + userID
+	r.save(key, bucket)
+}
+
+func (r *RedisStore) save(key string, bucket *model.TokenBucket){
+	bytes, _ := json.Marshal(bucket)
+
+	r.client.Set(r.ctx, key, bytes, time.minute)
 }
